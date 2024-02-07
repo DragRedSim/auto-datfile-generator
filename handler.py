d183fc47d7dd37c7e4ff108dc98ebd7586c33673
David Ross Smith <5095074+DragRedSim@users.noreply.github.com>
2024-02-07 22:52:57 +1100

    Added _cleanup_files() (useful for local runs)

--- handler.py
+++ handler.py
@@ -43,16 +43,24 @@ class dat_handler(metaclass=abc.ABCMeta):
         pass
 
     def __init__(self):
+    
+        if (self.CREATE_SOURCE_PKG):
+            self.XML_SOURCE_FILENAME        = self.XML_FILENAME[:-4]+"-source"+self.XML_FILENAME[-4:]
+            self.tag_clrmamepro_source      = ET.Element("clrmamepro")
+        else:
+            self.XML_SOURCE_FILENAME        = None
+            self.tag_clrmamepro_source      = None
+        
+        self._cleanup_files()
         self.tag_clrmamepro     = ET.Element("clrmamepro")
         self.pack_gen_date      = strftime("%Y-%m-%d", gmtime())
-        self.zip_object         = zipfile.ZipFile(self.ZIP_FILENAME, "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9)
-        if (self.CREATE_SOURCE_PKG):
-            self.XML_SOURCE_FILENAME = self.XML_FILENAME[:-4]+"-source"+self.XML_FILENAME[-4:]
-            self.tag_clrmamepro_source = ET.Element("clrmamepro")
-        if (os.getenv("GITHUB_REPOSITORY") != None):
-            self.ZIP_URL = f'https://github.com/{os.getenv("GITHUB_REPOSITORY")}/releases/{os.getenv("GITHUB_REF_NAME")}/download/{self.ZIP_FILENAME}'
+        
+        if (os.getenv("GITHUB_REPOSITORY") != None): #if we are running as part of a Github workflow, produce the URL using the account which is running the workflow
+            self.ZIP_URL        = f'https://github.com/{os.getenv("GITHUB_REPOSITORY")}/releases/{os.getenv("GITHUB_REF_NAME")}/download/{self.ZIP_FILENAME}'
         else:
-            self.ZIP_URL = None
+            self.ZIP_URL        = None
+            
+        self.zip_object         = zipfile.ZipFile(self.ZIP_FILENAME, "w", compression=zipfile.ZIP_DEFLATED, compresslevel=9)
     
     @abc.abstractmethod    
     def handle_file(self, dat_file):
@@ -63,3 +71,10 @@ class dat_handler(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def update_XML(self):
         raise NotImplementedError("Interface violation on update_XML()")
+    
+    def _cleanup_files(self):
+        for f in [self.ZIP_FILENAME or None, self.XML_FILENAME or None, self.XML_SOURCE_FILENAME or None]:
+            if f is not None:
+                g = os.path.abspath(f)
+                if (os.path.exists(g)):
+                    os.remove(g)
